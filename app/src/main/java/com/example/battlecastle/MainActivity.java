450a361045a7a70787ab38fde2e75d8924aee574
Khouw Devin Nathanael <khouwdevin@gmail.com>
2021-07-23 11:02:33 +0700

    new_update

--- app/src/main/java/com/example/battlecastle/MainActivity.java
+++ app/src/main/java/com/example/battlecastle/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -14,6 +15,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     Button battle_btn, sim1_btn, sim2_btn;
+    TextView hero_player1, hero_player2;
     ImageView p1_char, p2_char;
     Heroes p1, p2;
     boolean p1Win;
@@ -30,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         p2_char = findViewById(R.id.player2_char);
         sim1_btn = findViewById(R.id.sim1_btn);
         sim2_btn = findViewById(R.id.sim2_btn);
+        hero_player1 = findViewById(R.id.hero_player1);
+        hero_player2 = findViewById(R.id.hero_player2);
 
         battle_btn.setOnClickListener(battle_listener);
         sim1_btn.setOnClickListener(sim1_listener);
@@ -41,32 +45,34 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
-            if(p1_heroestype == 1){
-                p1 = new HeroesArcher(p1_heroestype, p1_armytype, p1_troops);
-            }
-            else if(p1_heroestype == 2){
-                p1 = new HeroesCavalry(p1_heroestype, p1_armytype, p1_troops);
-            }
-            else {
-                p1 = new HeroesInfantry(p1_heroestype, p1_armytype, p1_troops);
-            }
-            if(p2_heroestype == 1){
-                p2 = new HeroesArcher(p2_heroestype, p2_armytype, p2_troops);
-            }
-            else if(p2_heroestype == 2){
-                p2 = new HeroesCavalry(p2_heroestype, p2_armytype, p2_troops);
-            }
-            else {
-                p2 = new HeroesInfantry(p2_heroestype, p2_armytype, p2_troops);
-            }
+            try {
+                if (p1_heroestype == 1) {
+                    p1 = new HeroesArcher(p1_heroestype, p1_armytype, p1_troops);
+                } else if (p1_heroestype == 2) {
+                    p1 = new HeroesCavalry(p1_heroestype, p1_armytype, p1_troops);
+                } else {
+                    p1 = new HeroesInfantry(p1_heroestype, p1_armytype, p1_troops);
+                }
+                if (p2_heroestype == 1) {
+                    p2 = new HeroesArcher(p2_heroestype, p2_armytype, p2_troops);
+                } else if (p2_heroestype == 2) {
+                    p2 = new HeroesCavalry(p2_heroestype, p2_armytype, p2_troops);
+                } else {
+                    p2 = new HeroesInfantry(p2_heroestype, p2_armytype, p2_troops);
+                }
 
-            p1Win = p1.Battle(p1, p2);
+                p1Win = p1.Battle(p1, p2);
 
-            if(p1Win == true){
-                Toast.makeText(MainActivity.this, "Player 1 Win", Toast.LENGTH_LONG).show();
+                if (p1Win == true) {
+                    Toast.makeText(MainActivity.this, "Player 1 Win", Toast.LENGTH_LONG).show();
+                    p1_char.setImageResource(R.drawable.youwin);
+                } else {
+                    Toast.makeText(MainActivity.this, "Player 2 Win", Toast.LENGTH_LONG).show();
+                    p2_char.setImageResource(R.drawable.youwin);
+                }
             }
-            else{
-                Toast.makeText(MainActivity.this, "Player 2 Win", Toast.LENGTH_LONG).show();
+            catch (Exception e){
+                Toast.makeText(MainActivity.this, "Please select the simulation", Toast.LENGTH_LONG);
             }
 
         }
@@ -80,6 +86,26 @@ public class MainActivity extends AppCompatActivity {
 
             p1_heroestype = 2;
             p2_heroestype = 3;
+
+            if(p1_heroestype == 1){
+                hero_player1.setText("Archer");
+            }
+            else if(p1_heroestype == 2){
+                hero_player1.setText("Cavalry");
+            }
+            else{
+                hero_player1.setText("Infantry");
+            }
+
+            if(p2_heroestype == 1){
+                hero_player2.setText("Archer");
+            }
+            else if(p2_heroestype == 2){
+                hero_player2.setText("Cavalry");
+            }
+            else{
+                hero_player2.setText("Infantry");
+            }
 
             p1_armytype = new int[]{2};
             p2_armytype = new int[]{3};
@@ -97,6 +123,26 @@ public class MainActivity extends AppCompatActivity {
 
             p1_heroestype = 3;
             p2_heroestype = 2;
+
+            if(p1_heroestype == 1){
+                hero_player1.setText("Archer");
+            }
+            else if(p1_heroestype == 2){
+                hero_player1.setText("Cavalry");
+            }
+            else{
+                hero_player1.setText("Infantry");
+            }
+
+            if(p2_heroestype == 1){
+                hero_player2.setText("Archer");
+            }
+            else if(p2_heroestype == 2){
+                hero_player2.setText("Cavalry");
+            }
+            else{
+                hero_player2.setText("Infantry");
+            }
 
             p1_armytype = new int[]{3};
             p2_armytype = new int[]{2, 1};
